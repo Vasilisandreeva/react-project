a16ac8de726703e9b2db17feefbd75a05ed9d19a
Vasilisa <vasilisandreeva00@gmail.com>
2021-08-12 20:45:17 +0300

    qq

---
+++ src/components/User.js
@@ -0,0 +1,4 @@
+export default function User({userItem}) {
+  return (
+      <div>{userItem.id} - {userItem.name}</div>)
+}
---
+++ src/components/users/Users.js
@@ -0,0 +1,18 @@
+import User from "../User";
+import {useEffect, useState} from "react";
+import {getUsers} from "../../services/user.service";
+
+export default function Users() {
+
+    let [users, setUsers] = useState([])
+
+useEffect(() => {
+    getUsers().then(response => setUsers(response))
+}, []);
+
+  return (
+    <div>{
+     users.map(userItem => <User key={userItem.id} userItem={userItem}/>)
+    }</div>
+  );
+}

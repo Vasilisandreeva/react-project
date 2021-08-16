0262aa5f1936725b5b2c9bebdd1c20f446e8c9b7
Vasilisa <vasilisandreeva00@gmail.com>
2021-08-16 15:58:40 +0300

    qq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import Users from "./components/users/Users";
 
 function App() {
   return (
     <div>
-
+      <Users/>
     </div>
   );
 }
 
 export default App;
+
+// отримати всіх юзерів з https://jsonplaceholder.typicode.com/ вивести на сторінку
+//     До кожного юзера отримати пости та вивести в межах User компоненти
+// До кожного поста отримати комментарі, та вивести в межах Post компоненти
